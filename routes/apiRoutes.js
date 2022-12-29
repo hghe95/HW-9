2c7f55600caafa354bf30a664c5398df8103fc26
hghe95 <hghe95@gmail.com>
2022-12-28 20:23:34 -0800

    changed routes in the attempt to fix issue

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,18 @@
-const express = require(`express`);
-const router = express.Router();
-const uuid = require(`uuid`);
-let data = require(`./db/db.json`);
+const { v4: uuidv4 } = require(`uuid`);
+let data = require(`../Develop/db/db.json`);
 
+module.exports = app => {
 app.get(`/api/notes`, (req, res) => res.json(data));
+//     console.log(`Performing GET request`);
+//     res.json(JSON.parse(fs.readFileSync(data, "utf-8"))
+// )});
 
 app.post(`/api/notes`, (req, res) => {
     const title = req.body.title;
     const text = req.body.text;
-    const file = { id: uuid.v4(), title: title, text: text };
+    const file = { id: uuidv4(), title: title, text: text };
     data.push(file);
+    console.log(`A new note has been added to the database!`)
     res.json(file);
 });
-
-module.exports = router;
+}
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,18 +1,13 @@
 const path = require(`path`);
-const router = require(`express`);
+//const express = require(`express`);
+//const router = express.Router();
 
 module.exports = app => {
-    app.get(`*`, (req, res) => {
-        res.sendFile(path.join(__dirname, `../public/index.html`));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, `../Develop/public/index.html`));
     });
 
-    app.get(`/notes`, (req, res) => {
-        res.sendFile(path.join(__dirname, '../../public/notes.html'));
+    app.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, `../Develop/public/notes.html`));
     });
-
-    app.get(`/`, (req, res) => {
-        res.sendFile(path.join(__dirname, `../public/index.html`));
-    })
-}
-
-module.exports = router;
+};
