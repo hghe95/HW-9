21704e8a2d1bca245ca71023698a47869b84d11c
hghe95 <hghe95@gmail.com>
2023-01-03 19:59:35 -0800

    Finally fixed stuck on index.html error

--- server.js
+++ server.js
@@ -5,11 +5,9 @@ const path = require(`path`);
 //require(`./routes/routes`)(app);
 // const apiRoutes = require(`./routes/apiRoutes`);
 // const htmlRoutes = require(`./routes/htmlRoutes`);
-let data = require(`./Develop/db/db.json`);
-
+let dataJSON = require(`./db/db.json`);
 // Localhost and app initializer
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
 // Middleware
@@ -21,28 +19,29 @@ app.use(express.static(`public`));
 //app.use(`/`, htmlRoutes);
 
 //HTML Routes
-app.get(`*`, (req, res) => {
-    res.sendFile(path.join(__dirname, `./Develop/public/index.html`));
-});
-
 app.get(`/`, (req, res) => {
-    res.sendFile(path.join(__dirname, `./Develop/public/index.html`));
+    res.sendFile(path.join(__dirname, `./public/index.html`));
 });
 
 app.get(`/notes`, (req, res) => {
-    res.sendFile(path.join(__dirname, `./Develop/public/notes.html`));
+    res.sendFile(path.join(__dirname, `./public/notes.html`));
 });
 
 //API Routes
-app.get(`/api/notes`, (req, res) => res.json(data));
+app.get(`/api/notes`, (req, res) => { res.json(dataJSON) });
 
 app.post(`/api/notes`, (req, res) => {
     const title = req.body.title;
     const text = req.body.text;
-    const file = { id: uuidv4(), title: title, text: text };
-    data.push(file);
+    const note = { id: uuidv4(), title: title, text: text };
+    dataJSON.push(note);
     console.log(`A new note has been added to the database!`)
-    res.json(file);
+    res.json(dataJSON);
+});
+
+//Wildcard Route
+app.get(`*`, (req, res) => {
+    res.sendFile(path.join(__dirname, `./public/index.html`));
 });
 
 // Listener
